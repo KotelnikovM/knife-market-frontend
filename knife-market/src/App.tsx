21c75403c74b90babc86ea5d4fc90ca7f8311273
KotelnikovM <kotelnikovofficial@gmail.com>
2023-09-12 21:21:27 +0500

    test

---
+++ knife-market/src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const getData = async () => {
+    return fetch(`http://localhost:8000/api/data`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+}
+
+export default App;
